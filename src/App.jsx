f44833620d2e51b74689b8ab3edaf352013fb8bd
Yuliia Shvets <y.shvets85@gmail.com>
2025-03-09 00:03:40 +0100

    add .

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import { useDispatch, useSelector } from "react-redux";
-import { lazy, Suspense, useEffect } from "react";
+import { lazy, useEffect } from "react";
 import { selectIsRefreshing } from "./redux/auth/selectors";
 import { refreshUser } from "./redux/auth/operations";
 import { Route, Routes } from "react-router-dom";
-import { Layout } from "./components/Layout";
-import { RestrictedRoute } from "./components/RestrictedRoute";
-import { PrivateRoute } from "./components/PrivateRoute";
+import { Layout } from "./components/Layout/Layout.jsx";
+import { RestrictedRoute } from "./components/RestrictedRoute/RestrictedRoute.jsx";
+import { PrivateRoute } from "./components/PrivateRoute/PrivateRoute.jsx";
 import s from "./App.module.css";
 
 const HomePage = lazy(() => import("./pages/HomePage/HomePage"));
-const RegistrationPage = lazy(() => import("./pages/RegistrationPage/RegistrationPage"));
+const RegistrationPage = lazy(
+  () => import("./pages/RegistrationPage/RegistrationPage")
+);
 const LoginPage = lazy(() => import("./pages/LoginPage/LoginPage"));
 const ContactsPage = lazy(() => import("./pages/ContactsPage/ContactsPage"));
 
@@ -26,8 +28,7 @@ const App = () => {
   }
 
   return (
-<div className={s.app}>
-    <Suspense fallback={<p>Loading...</p>}>
+    <div className={s.app}>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<HomePage />} />
@@ -60,7 +61,6 @@ const App = () => {
           />
         </Route>
       </Routes>
-    </Suspense>
     </div>
   );
 };
--- src/components/PrivateRoute/PrivateRoute.jsx
+++ src/components/PrivateRoute/PrivateRoute.jsx
@@ -1,6 +1,6 @@
 import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
-import { selectIsLoggedIn, selectIsRefreshing } from "../redux/auth/selectors";
+import { selectIsLoggedIn, selectIsRefreshing } from "../../redux/auth/selectors";
 
 export const PrivateRoute = ({ children, redirectTo = "/" }) => {
   const isLoggedIn = useSelector(selectIsLoggedIn);
